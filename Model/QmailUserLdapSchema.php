a3d50e5d74305691ffd57fe71a82e2f111a369ca
Mike Manske <mmanske2@gmail.com>
2017-05-11 17:15:02 -0500

    First commit

---
+++ Model/QmailUserLdapSchema.php
@@ -0,0 +1,75 @@
+<?php
+/**
+* User plugin to export to the qmailUser mailAlternateAddress. It requires a an extendedtype
+* email type called 'altListSub' to be added into COmanage. Also, your LDAP server will have
+* to know about the qmailUser schema.
+*
+*/
+class QmailUserLdapSchema extends AppModel
+{
+
+  // using qmail
+  public $attributes = array(
+    'qmailUser' => array(
+      'objectclass' => array(
+        'required' => true
+      ),
+      'attributes' => array(
+        'mailAlternateAddress' => array(
+          'required' => false,
+          'multiple' => true,
+          'extendedtype' => 'email_address_types',
+          'defaulttype' => 'altListSub'
+        )
+      )
+    )
+  );
+
+  // Required by COmanage Plugins
+  public $cmPluginType = "ldapschema";
+
+  // Document foreign keys
+  public $cmPluginHasMany = array();
+
+  /**
+  * Assemble attributes to write. Required for LDAP schema plugin.
+  *
+  * @since COmanage Registry 1.1.0
+  * @param array $configuredAttributes Array of configured attributes
+  * @param array $provisioningData Array of provisioning data
+  * @return array Array of attribute names and values to write
+  */
+  public function assemblePluginAttributes($configuredAttributes, $provisioningData)
+  {
+    $attrs = array( );
+//  return $attrs; There should be only but but we loop for forms sake.
+    foreach ($configuredAttributes as $attr => $cfg) {
+        if ($attr == 'mailAlternateAddress') {
+            $attrs[$attr] = array();
+            $this->log("Found mailAlternateAddress " . print_r($cfg, true));
+            foreach ($provisioningData['EmailAddress'] as $emailAddy) {
+                $this->log("Found EmailAddress " . print_r($emailAddy, true));
+
+                if (isset($cfg['export']) && $cfg['export'] == 1 && isset($emailAddy['type'])
+                   && $emailAddy['type'] == $cfg['type']) {
+                    $attrs[$attr][] = $emailAddy['mail'];
+                }
+            }
+        }
+    }
+
+      $this->log("Returning these email addresses" . print_r($attrs, true));
+      return $attrs;
+  }
+
+  /**
+   * Expose menu items.
+   *
+   * @ since COmanage Registry v0.9.2
+   * @ return Array with menu location type as key and array of labels, controllers, actions as values.
+   */
+  public function cmPluginMenus()
+  {
+      return array();
+  }
+}
